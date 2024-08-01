c5d0347f467a26ea41491c10e56937c1438d4d8c
Justin Kahn <jdavidkahn@gmail.com>
2024-08-01 14:10:00 -0500

    adding both clickable branches

--- src/components/AddEmployee/AddEmployee.jsx
+++ src/components/AddEmployee/AddEmployee.jsx
@@ -1,22 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import "./AddEmployee.css"
+import './AddEmployee.css';
 import ToggleEmployee from './ToggleEmployee';
-
 
 const AddEmployee = (props) => {
     const dispatch = useDispatch();
     const employees = useSelector((state) => state.addEmployeeReducer);
 
-
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [employeeNumber, setEmployeeNumber] = useState('');
     const [unionName, setUnionName] = useState(''); 
-
-    // const [employeeStatus, setEmployeeStatus] = useState(true);
-
     const [phoneNumber, setPhoneNumber] = useState('');
     const [email, setEmail] = useState('');
     const [address, setAddress] = useState('');
@@ -33,7 +28,6 @@ const AddEmployee = (props) => {
             first_name: firstName,
             last_name: lastName,
             employee_number: employeeNumber,
-
             union_name: unionName,
             phone_number: phoneNumber,
             email,
@@ -41,7 +35,6 @@ const AddEmployee = (props) => {
         };
         dispatch({ type: 'ADD_EMPLOYEE_INFO', payload: newEmployee });
 
-        // Clear the form
         setFirstName('');
         setLastName('');
         setEmployeeNumber('');
@@ -52,15 +45,46 @@ const AddEmployee = (props) => {
     };
 
     const handleEditClick = (emp) => {
-
-
         dispatch({ type: 'SET_EDIT_EMPLOYEE', payload: emp });
         history.push('/editemployee');
     };
 
+    const fillDummyDataAndSubmit = () => {
+        const dummyData = {
+            firstName: 'Emily',
+            lastName: 'Smith',
+            employeeNumber: 'A12345',
+            unionName: '22 - Cement Masons/Finishers',
+            phoneNumber: '(555) 678-1234',
+            email: 'emily.smith@company.com',
+            address: '456 Elm Street, Springfield, IL'
+        };
+
+        setFirstName(dummyData.firstName);
+        setLastName(dummyData.lastName);
+        setEmployeeNumber(dummyData.employeeNumber);
+        setUnionName(dummyData.unionName);
+        setPhoneNumber(dummyData.phoneNumber);
+        setEmail(dummyData.email);
+        setAddress(dummyData.address);
+
+        dispatch({
+            type: 'ADD_EMPLOYEE_INFO',
+            payload: {
+                first_name: dummyData.firstName,
+                last_name: dummyData.lastName,
+                employee_number: dummyData.employeeNumber,
+                union_name: dummyData.unionName,
+                phone_number: dummyData.phoneNumber,
+                email: dummyData.email,
+                address: dummyData.address
+            }
+        });
+    };
+
     return (
         <>
-            <h2 className='employee-title'>Add Employee</h2>
+            <h2 className='employee-title' onClick={fillDummyDataAndSubmit}>Add Employee</h2>
 
             <form className='employee-inputs' onSubmit={handleSubmit}>
                 <input
@@ -84,25 +108,22 @@ const AddEmployee = (props) => {
                     value={employeeNumber}
                     onChange={(event) => setEmployeeNumber(event.target.value)}
                 />
-<div>
-            <label htmlFor="union_name">Select Union:</label>
-            <select
-                id="union_name"
-                name="union_name"
-                value={unionName}
-                onChange={(event) => setUnionName(event.target.value)}
-            >
-                <option value="" disabled>Select a union</option>
-                <option value="21 - Bricklayers">21 - Bricklayers</option>
-                <option value="22 - Cement Masons/Finishers">22 - Cement Masons/Finishers</option>
-                <option value="23 - Laborers">23 - Laborers</option>
-                <option value="24 - Operators">24 - Operators</option>
-                <option value="25 - Carpenters">25 - Carpenters</option>
-            </select>
-        </div>
-
-
-
+                <div>
+                    <label htmlFor="union_name">Select Union:</label>
+                    <select
+                        id="union_name"
+                        name="union_name"
+                        value={unionName}
+                        onChange={(event) => setUnionName(event.target.value)}
+                    >
+                        <option value="" disabled>Select a union</option>
+                        <option value="21 - Bricklayers">21 - Bricklayers</option>
+                        <option value="22 - Cement Masons/Finishers">22 - Cement Masons/Finishers</option>
+                        <option value="23 - Laborers">23 - Laborers</option>
+                        <option value="24 - Operators">24 - Operators</option>
+                        <option value="25 - Carpenters">25 - Carpenters</option>
+                    </select>
+                </div>
                 <input
                     type="text"
                     name="phone_number"
@@ -134,9 +155,7 @@ const AddEmployee = (props) => {
                         <th>First Name</th>
                         <th>Employee Number</th>
                         <th>Union</th>
-
                         <th>Employee Status</th>
-
                         <th>Phone Number</th>
                         <th>Email</th>
                         <th>Address</th>
@@ -151,8 +170,7 @@ const AddEmployee = (props) => {
                             <td>{emp.employee_number}</td>
                             <td>{emp.union_name}</td>
                             <td>
-                                <ToggleEmployee
-                                    emp={emp} />
+                                <ToggleEmployee emp={emp} />
                             </td>
                             <td>{emp.phone_number}</td>
                             <td>{emp.email}</td>
@@ -167,4 +185,3 @@ const AddEmployee = (props) => {
 };
 
 export default AddEmployee;
-
